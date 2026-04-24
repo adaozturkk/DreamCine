1f928b354571993a3af204c5034c53e432facdf9
Ada Öztürk <adaozturk@icloud.com>
2026-04-24 20:51:15 +0300

    Add ApplicationDbContext

---
+++ DreamCine.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DreamCine.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DreamCine.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieGenre> MovieGenres { get; set; }
+        public DbSet<MovieSession> MovieSessions { get; set; }
+        public DbSet<Screen> Screens { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MovieGenre>()
+                .HasKey(mg => new { mg.MovieId, mg.GenreId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
