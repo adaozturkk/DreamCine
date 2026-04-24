b71818432bee9b89ceaab5ebcee9836f07d53879
Ada Öztürk <adaozturk@icloud.com>
2026-04-24 20:33:45 +0300

    Add related models

---
+++ DreamCine.Api/Models/MovieSession.cs
@@ -0,0 +1,12 @@
+﻿namespace DreamCine.Api.Models
+{
+    public class MovieSession
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+        public int ScreenId { get; set; }
+        public Screen Screen { get; set; } = null!;
+        public DateTime SessionTime { get; set; }
+    }
+}
---
+++ DreamCine.Api/Models/Seat.cs
@@ -0,0 +1,11 @@
+﻿namespace DreamCine.Api.Models
+{
+    public class Seat
+    {
+        public int Id { get; set; }
+        public int ScreenId { get; set; }
+        public Screen Screen { get; set; } = null!;
+        public string SeatNumber { get; set; } = string.Empty;
+
+    }
+}
