c989263b2532131fa30b8f6e00213639fc03a799
Ada Öztürk <adaozturk@icloud.com>
2026-05-01 16:26:24 +0300

    Add screen controller

---
+++ DreamCine.Api/Controllers/ScreensController.cs
@@ -0,0 +1,75 @@
+﻿using DreamCine.Api.DTOs.Screen;
+using DreamCine.Api.Interfaces;
+using DreamCine.Api.Mappers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DreamCine.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScreensController : ControllerBase
+    {
+        private readonly IScreenRepository _screenRepo;
+
+        public ScreensController(IScreenRepository screenRepo)
+        {
+            _screenRepo = screenRepo;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateScreen([FromBody] CreateScreenDto screenDto)
+        {
+            var screen = await _screenRepo.CreateAsync(screenDto.ToScreenFromCreateDto());
+            return Ok(screen.ToScreenDto());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var screens = await _screenRepo.GetAllAsync();
+            var screenDtos = screens.Select(x => x.ToScreenDto()).ToList();
+
+            return Ok(screenDtos);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var screen = await _screenRepo.GetByIdAsync(id);
+
+            if (screen == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(screen.ToScreenDto());
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateScreenDto updateDto)
+        {
+            var screen = await _screenRepo.UpdateAsync(id, updateDto.ToScreenFromUpdateDto());
+
+            if (screen == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(screen.ToScreenDto());
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var screen = await _screenRepo.DeleteAsync(id);
+
+            if (screen == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
