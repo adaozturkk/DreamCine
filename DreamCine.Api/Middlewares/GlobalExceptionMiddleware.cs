36cb1505e82e9164d535745d8d7a6f8a5947a8d9
Ada Öztürk <adaozturk@icloud.com>
2026-06-28 18:26:05 +0300

    implement global exception handling middleware

---
+++ DreamCine.Api/Middlewares/GlobalExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace DreamCine.Api.Middlewares
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+        public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An uncaught error occurred in the system!");
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = new
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = "An unexpected error occurred on the server side. Please try again later.",
+                    Detailed = ex.Message // for satefy delete this before publish!!!
+                };
+
+                var jsonResponse = JsonSerializer.Serialize(response);
+                await context.Response.WriteAsync(jsonResponse);
+            }
+        }
+    }
+}
--- DreamCine.Api/Program.cs
+++ DreamCine.Api/Program.cs
@@ -110,6 +110,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<DreamCine.Api.Middlewares.GlobalExceptionMiddleware>();
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
