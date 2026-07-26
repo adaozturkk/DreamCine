1c4d3b96727e7e8faf05a56353e5b89c6404e2b9
Ada Öztürk <adaozturk@icloud.com>
2026-07-26 19:05:19 +0300

    add AuthResponseDto and IAccountService for refactoring

---
+++ DreamCine.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using DreamCine.Application.Common;
+using DreamCine.Application.DTOs.Account;
+
+namespace DreamCine.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<ServiceResult<string>> RegisterAsync(RegisterDto registerDto);
+        Task<ServiceResult<AuthResponseDto>> LoginAsync(LoginDto loginDto);
+        Task<ServiceResult<AuthResponseDto>> RefreshTokenAsync(TokenDto tokenDto);
+        Task<ServiceResult<string>> AssignRoleAsync(AssignRoleDto roleDto);
+        Task<ServiceResult<string>> ForgotPasswordAsync(ForgotPasswordDto passwordDto);
+        Task<ServiceResult<string>> ResetPasswordAsync(ResetPasswordDto passwordDto);
+        Task<ServiceResult<string>> ChangePasswordAsync(ChangePasswordDto passwordDto, string userEmail);
+    }
+}
